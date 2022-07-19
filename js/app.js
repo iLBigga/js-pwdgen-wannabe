201262a508c971eaffdc0bcdba73f9c89a23bc0c
iLBigga <Giannanti1996@gmail.com>
2022-07-19 17:06:40 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Nome
+
+const mioNome = prompt('Come ti chiami?')
+console.log(mioNome)
+
+// Cognome
+
+const mioCognome = prompt('Qual è il tuo cognome?')
+
+// Colore preferito
+
+const colorePreferito = prompt('Qual è il tuo colore preferito?')
+
+// Get element
+
+const title = document.getElementById('title')
+
+// Password
+
+const passWorld = mioNome + mioCognome + colorePreferito + 246
+
+// Risultato
+
+title.innerHTML = passWorld
